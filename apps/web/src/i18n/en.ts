02dfc8bcd0fef5ecc8cb637cf8cd865b566a4404
Ruben <rubdeivis@gmail.com>
2024-09-26 20:00:28 -0400

    build: Init tailwind, firebase and i18n

--- apps/web/src/i18n/en.ts
+++ apps/web/src/i18n/en.ts
@@ -15,4 +15,22 @@ export default {
     photos: "Photos",
     see_all_reviews: "See all reviews",
   },
+  login: {
+    login_title: "Login into <name>",
+    login_with_google: "Login with google",
+    email: "Email",
+    password: "Password",
+    login: "Log In",
+    dont_you_have_account: "Don't you have an account?",
+    sign_up: "Sign up",
+  },
+  signup: {
+    title: "Signup to <app name> ",
+    signup_with_google: "Sign up with google",
+    email: "Email",
+    password: "Password",
+    signup: "Sign up",
+    login: "Log in",
+    already_have_account: "Already have an account?",
+  },
 };
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,8 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
-// https://vitejs.dev/config/
+const env = process.env;
+
 export default defineConfig({
+  define: {
+    // this env comes from global turborepo .env
+    "process.env": JSON.stringify({
+      VITE_FIREBASE_API_KEY: env.FIREBASE_API_KEY,
+      VITE_FIREBASE_AUTH_DOMAIN: env.FIREBASE_AUTH_DOMAIN,
+      VITE_FIREBASE_PROJETCT_ID: env.FIREBASE_PROJETCT_ID,
+      VITE_FIREBASE_STORAGE_BUCKET: env.FIREBASE_STORAGE_BUCKET,
+      VITE_FIREBASE_APP_ID: env.FIREBASE_APP_ID,
+      VITE_FIREBASE_MESSAGE_SENDER_ID: env.FIREBASE_MESSAGE_SENDER_ID,
+    }),
+  },
   plugins: [TanStackRouterVite(), react()],
   server: {
     port: 5000,
